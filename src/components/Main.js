6795b285da04e8a2310719a56e6023e0913892ec
Josh <josh.asi@hotmail.co.nz>
2019-10-01 09:29:11 +1300

    Changed the icon to be more suitable
    Removed unused styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import wdhbLogo from '../media/WDHB_Logo.svg';
-import netflixLogo from '../media/netflix.png';
-import spotifyLogo from '../media/spotify.png';
-import gameIcon from '../media/games.svg';
+import wdhbLogo from "../media/WDHB_Logo.svg";
+import tvIcon from "../media/retro-tv.svg";
+import musicIcon from "../media/music-solid.svg";
+import diceIcon from "../media/dice-solid.svg";
 
 const Main = () => {
-	return (
-		<main className="interface">
-			<header className="interface__header">
-				<img src={wdhbLogo} alt="WDHB Logo" className="interface__logo wdhb-logo" />
-				<h1 className="heading-primary">Welcome to Lakeview</h1>
-			</header>
+  return (
+    <main className="interface">
+      <header className="interface__header">
+        <img
+          src={wdhbLogo}
+          alt="WDHB Logo"
+          className="interface__logo wdhb-logo"
+        />
+        <h1 className="heading-primary">Welcome to Lakeview</h1>
+      </header>
 
-			<div className="interface__apps">
-				<a className="interface__app interface__app--netflix" href="nflx://">
-					<img src={netflixLogo} alt="Netflix logo" className="logo netflix-logo" />
-					Watch
-				</a>
-				<a className="interface__app interface__app--spotify" href="spotify://">
-					<img src={spotifyLogo} alt="Spotify logo" className="logo spotify-logo" />
-					Listen
-				</a>
+      <div className="interface__apps">
+        <a className="interface__app interface__app--netflix" href="nflx://">
+          <img src={tvIcon} alt="TV Icon" className="icon icon--tv" />
+          Watch
+        </a>
+        <a className="interface__app interface__app--spotify" href="spotify://">
+          <img src={musicIcon} alt="Music Icon" className="icon icon--music" />
+          Listen
+        </a>
 
-				<Link className="interface__app interface__app--games" to="/games">
-					<img src={gameIcon} alt="Game icon" className="logo game-icon" />
-					Play
-				</Link>
-			</div>
-		</main>
-	);
+        <Link className="interface__app interface__app--games" to="/games">
+          <img src={diceIcon} alt="Game icon" className="icon icon--game" />
+          Play
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default Main;
